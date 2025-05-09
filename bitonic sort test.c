38e34b34b0833c7f51b93d3aae98a7a97ff6d68f
Nathan <nathan.landau1234@gmail.com>
2025-05-10 00:06:50 +0200

    tested bitonic sort

--- bitonic sort test.c
+++ bitonic sort test.c
@@ -13,8 +13,10 @@ void print_m256i_as_int16(__m256i vec) {
 }
 
 __m256i bitonic_sort_epu16(__m256i input) {
+    __m256i inverted = _mm256_permute4x64_epi64(input, _MM_SHUFFLE(1, 0, 3, 2));
+    print_m256i_as_int16(inverted);
     // step 1
-    __m256i inverted = _mm256_permute2x128_si256(input, input, 0x21);
+    inverted = _mm256_permute2x128_si256(input, input, 0x21);
     input = _mm256_permute2x128_si256(_mm256_max_epi16(input, inverted), _mm256_min_epi16(input, inverted), 0x30);
     print_m256i_as_int16(input);
 
@@ -52,10 +54,10 @@ __m256i bitonic_sort_epu16(__m256i input) {
 
 // test the bitonic sort function
 int main() {
-    __m256i input = _mm256_set_epi16(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
-    __m256i input2 = _mm256_set_epi16(16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
-    __m256i input3 = _mm256_set_epi16(8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0);
-    __m256i input4 = _mm256_set_epi16(0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8);
+    __m256i input = _mm256_setr_epi16(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+    __m256i input2 = _mm256_setr_epi16(16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+    __m256i input3 = _mm256_setr_epi16(8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0);
+    __m256i input4 = _mm256_setr_epi16(0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8);
     __m256i sorted = bitonic_sort_epu16(input);
     printf("\n");
     __m256i sorted2 = bitonic_sort_epu16(input2);
