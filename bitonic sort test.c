8a1a3c764e31cc6710daec940ae5b890ec5ed205
Nathan <nathan.landau1234@gmail.com>
2025-05-10 00:07:10 +0200

    tested bitonic sort

--- bitonic sort test.c
+++ bitonic sort test.c
@@ -13,6 +13,7 @@ void print_m256i_as_int16(__m256i vec) {
 }
 
 __m256i bitonic_sort_epu16(__m256i input) {
+    print_m256i_as_int16(input);
     __m256i inverted = _mm256_permute4x64_epi64(input, _MM_SHUFFLE(1, 0, 3, 2));
     print_m256i_as_int16(inverted);
     // step 1
