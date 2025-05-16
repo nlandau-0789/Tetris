34d09fd89355d60878f17ba1dc7d738dba56c7da
Nathan <nathan.landau1234@gmail.com>
2025-05-16 21:08:35 +0200

    tested bitonic sort

--- bitonic sort test.c
+++ bitonic sort test.c
@@ -23,7 +23,7 @@ __m256i bitonic_sort_epu16(__m256i input) {
     input = _mm256_blend_epi16(
         _mm256_max_epu16(input, inverted),
         _mm256_min_epu16(input, inverted),
-        0x99
+        0x66
     );
     print_m256i_as_int16(input);
     return input;
