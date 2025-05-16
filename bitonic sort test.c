e99f9dc1ea358494667c5313398e9fdcd189404e
Nathan <nathan.landau1234@gmail.com>
2025-05-16 21:24:35 +0200

    tested bitonic sort

--- bitonic sort test.c
+++ bitonic sort test.c
@@ -58,11 +58,28 @@ __m256i bitonic_sort_epu16(__m256i input) {
 
     // step 5
     inverted = _mm256_shuffle_epi32(input, _MM_SHUFFLE(2, 3, 0, 1));
-    print_m256i_as_int16(inverted);
     input = _mm256_blend_epi32(
         _mm256_max_epu16(input, inverted),
         _mm256_min_epu16(input, inverted),
         0x5a
+    );
+    print_m256i_as_int16(input);
+
+    // step 6
+    inverted = _mm256_shufflehi_epi16(input, _MM_SHUFFLE(2, 3, 0, 1));
+    inverted = _mm256_shufflelo_epi16(inverted, _MM_SHUFFLE(2, 3, 0, 1));
+    input = _mm256_permute2x128_si256(
+        _mm256_blend_epi16(
+            _mm256_max_epu16(input, inverted),
+            _mm256_min_epu16(input, inverted),
+            0x66
+        ), 
+        _mm256_blend_epi16(
+            _mm256_max_epu16(input, inverted),
+            _mm256_min_epu16(input, inverted),
+            0x99
+        ), 
+        0x21
     );
     print_m256i_as_int16(input);
 
