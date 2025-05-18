f687c4706ed427ee20e7ae094310f76079ce3305
Nathan <nathan.landau1234@gmail.com>
2025-05-18 12:42:41 +0200

    tested v5

--- v5.c
+++ v5.c
@@ -177,6 +177,121 @@ __m256i board_from_array(bool *t){
     return result;
 }
 
+__m256i bitonic_sort_epu16(__m256i input) {
+    // print_m256i_as_int16(input);
+    __m256i inverted;
+
+    // 2
+    // step 1
+    inverted = _mm256_shufflehi_epi16(input, _MM_SHUFFLE(2, 3, 0, 1));
+    inverted = _mm256_shufflelo_epi16(inverted, _MM_SHUFFLE(2, 3, 0, 1));
+    input = _mm256_blend_epi16(
+        _mm256_max_epu16(input, inverted),
+        _mm256_min_epu16(input, inverted),
+        0x66
+    );
+    // print_m256i_as_int16(input);
+
+    // 4
+    // step 2
+    inverted = _mm256_shuffle_epi32(input, _MM_SHUFFLE(2, 3, 0, 1));
+    input = _mm256_blend_epi16(
+        _mm256_max_epu16(input, inverted),
+        _mm256_min_epu16(input, inverted),
+        0x3c
+    );
+    // print_m256i_as_int16(input);
+
+    // step 3
+    inverted = _mm256_shufflehi_epi16(input, _MM_SHUFFLE(2, 3, 0, 1));
+    inverted = _mm256_shufflelo_epi16(inverted, _MM_SHUFFLE(2, 3, 0, 1));
+    input = _mm256_blend_epi16(
+        _mm256_max_epu16(input, inverted),
+        _mm256_min_epu16(input, inverted),
+        0x5a
+    );
+    // print_m256i_as_int16(input);
+
+    // 8
+    // step 4
+    inverted = _mm256_permute4x64_epi64(input, _MM_SHUFFLE(2, 3, 0, 1));
+    input = _mm256_blend_epi32(
+        _mm256_max_epu16(input, inverted),
+        _mm256_min_epu16(input, inverted),
+        0x3c
+    );
+    // print_m256i_as_int16(input);
+
+    // step 5
+    inverted = _mm256_shuffle_epi32(input, _MM_SHUFFLE(2, 3, 0, 1));
+    input = _mm256_blend_epi32(
+        _mm256_max_epu16(input, inverted),
+        _mm256_min_epu16(input, inverted),
+        0x5a
+    );
+    // print_m256i_as_int16(input);
+
+    // step 6
+    inverted = _mm256_shufflehi_epi16(input, _MM_SHUFFLE(2, 3, 0, 1));
+    inverted = _mm256_shufflelo_epi16(inverted, _MM_SHUFFLE(2, 3, 0, 1));
+    __m256i minvec = _mm256_min_epu16(input, inverted);
+    __m256i maxvec = _mm256_max_epu16(input, inverted);
+    input = _mm256_permute2x128_si256(
+        _mm256_blend_epi16(
+            maxvec,
+            minvec,
+            0x55
+        ), 
+        _mm256_blend_epi16(
+            maxvec,
+            minvec,
+            0xaa
+        ), 
+        0x12
+    );
+    // print_m256i_as_int16(input);
+    
+    // 16
+    // step 7
+    inverted = _mm256_permute2x128_si256(input, input, 0x21);
+    input = _mm256_permute2x128_si256(
+        _mm256_max_epu16(input, inverted),
+        _mm256_min_epu16(input, inverted),
+        0x21
+    );
+    // print_m256i_as_int16(input);
+
+    // step 8
+    inverted = _mm256_permute4x64_epi64(input, _MM_SHUFFLE(2, 3, 0, 1));
+    input = _mm256_blend_epi32(
+        _mm256_max_epu16(input, inverted),
+        _mm256_min_epu16(input, inverted),
+        0xcc
+    );
+    // print_m256i_as_int16(input);
+
+    // step 9
+    inverted = _mm256_shuffle_epi32(input, _MM_SHUFFLE(2, 3, 0, 1));
+    input = _mm256_blend_epi16(
+        _mm256_max_epu16(input, inverted),
+        _mm256_min_epu16(input, inverted),
+        0xcc
+    );
+    // print_m256i_as_int16(input);
+
+    // step 10
+    inverted = _mm256_shufflehi_epi16(input, _MM_SHUFFLE(2, 3, 0, 1));
+    inverted = _mm256_shufflelo_epi16(inverted, _MM_SHUFFLE(2, 3, 0, 1));
+    input = _mm256_blend_epi16(
+        _mm256_max_epu16(input, inverted),
+        _mm256_min_epu16(input, inverted),
+        0xaa
+    );
+    // print_m256i_as_int16(input);
+
+    return input;
+}
+
 __m256i log2_epi16(__m256i input) {
     __m256i result = _mm256_set1_epi16(16);
     __m256i zero = _mm256_setzero_si256();
@@ -261,12 +376,12 @@ void calc_consts(__m256i input) {
 
     __m256i rotated_right = rotate_right_one(heights);
     __m256i rotated_left = rotate_left_one(heights);
-    __m256i max_height = max_all_epi16(heights);
-    __m256i max_holes = max_all_epi16(holes);
+    __m256i max_height = bitonic_sort_epu16(heights);
+    __m256i max_holes = bitonic_sort_epu16(holes);
     __m256i height_diffs = cut_epi16(_mm256_abs_epi16(_mm256_sub_epi16(rotated_left, heights)), 16-9);
     // __m256i sum_diffs = add_all_epi16(height_diffs);
-    __m256i max_diff = max_all_epi16(height_diffs);
-    __m256i max_cav = max_all_epi16(cavities);
+    __m256i max_diff = bitonic_sort_epu16(height_diffs);
+    __m256i max_cav = bitonic_sort_epu16(cavities);
     __m256i wells_deepness = cut_epi16(_mm256_min_epu16(_mm256_subs_epu16(rotated_right, heights), _mm256_subs_epu16(rotated_left, heights)), 16-10);
     __m256i wells_mask = _mm256_cmpgt_epi16(wells_deepness, two);
     __m256i wells = _mm256_and_si256(wells_mask, wells_deepness);
